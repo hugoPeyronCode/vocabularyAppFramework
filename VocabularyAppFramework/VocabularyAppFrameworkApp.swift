6c00450713858246b20c90da7a86ef93b3145d6c
Hugo Peyron <hugopeyron@gmail.com>
2023-09-19 14:38:58 +0200

    Initial Commit

---
+++ VocabularyAppFramework/VocabularyAppFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabularyAppFrameworkApp.swift
+//  VocabularyAppFramework
+//
+//  Created by Hugo Peyron on 19/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct VocabularyAppFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
